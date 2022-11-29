75385e4c8fdadebd96424c40320eda640ef4a58f
Corey Sawin <sawincp@gmail.com>
2022-11-29 11:30:17 -0500

    commit tree 7a3026d5acd5756623d5273ea0a26ccbe4841299

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(name){
+    cats.push("Ralph");
+}
+function destructivelyPrependCat(name){
+    cats.unshift("Bob");
+}
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+function appendCat(name){
+   const copyOfCats = [...cats, name];
+   return copyOfCats;
+}
+
+function prependCat(name){
+    const copyOfCats = [name, ...cats]
+    return copyOfCats;
+}
+function removeLastCat(){
+    const copyOfCats = cats.slice(0, cats.length - 1);
+    return copyOfCats;
+}
+function removeFirstCat(){
+    const copyOfCats = cats.slice(1);
+    return copyOfCats;
+}
